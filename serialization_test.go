eacb6807e08ca7ea52e2c9b45a5e4fbefc9b1757
zonewave <ybzhang17@163.com>
2023-04-14 16:30:23 +0800

    sets: support json marshal/unmarshal. (#22)

---
+++ serialization_test.go
@@ -0,0 +1,53 @@
+package set
+
+import (
+	"encoding/json"
+	"github.com/shoenig/test/must"
+	"testing"
+)
+
+func TestSerialization(t *testing.T) {
+	t.Run("Set", func(t *testing.T) {
+		set := New[int](3)
+		set.InsertSlice([]int{1, 2, 3})
+		bs, err := json.Marshal(set)
+		must.NoError(t, err)
+		must.StrContains(t, string(bs), "1")
+		must.StrContains(t, string(bs), "2")
+		must.StrContains(t, string(bs), "3")
+
+		dstSet := New[int](3)
+		err = json.Unmarshal(bs, dstSet)
+		must.NoError(t, err)
+		must.MapEq(t, dstSet.items, set.items)
+	})
+	t.Run("HashSet", func(t *testing.T) {
+		set := NewHashSet[*company, string](10)
+		set.InsertSlice([]*company{c1, c2, c3})
+		bs, err := json.Marshal(set)
+		must.NoError(t, err)
+		must.StrContains(t, string(bs), `"street":1`)
+		must.StrContains(t, string(bs), `"street":2`)
+		must.StrContains(t, string(bs), `"street":3`)
+
+		dstSet := NewHashSet[*company, string](10)
+		err = json.Unmarshal(bs, dstSet)
+		must.NoError(t, err)
+		must.MapEqual(t, dstSet.items, set.items)
+	})
+	t.Run("TreeSet", func(t *testing.T) {
+		set := NewTreeSet[int, Compare[int]](Cmp[int])
+		set.InsertSlice([]int{10, 3, 13})
+		bs, err := json.Marshal(set)
+		must.NoError(t, err)
+		must.StrContains(t, string(bs), "10")
+		must.StrContains(t, string(bs), "3")
+		must.StrContains(t, string(bs), "13")
+
+		dstSet := NewTreeSet[int, Compare[int]](Cmp[int])
+		err = json.Unmarshal(bs, dstSet)
+		must.NoError(t, err)
+		must.Eq(t, set.Slice(), dstSet.Slice())
+	})
+
+}
--- set_test.go
+++ set_test.go
@@ -4,6 +4,7 @@
 package set
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 
@@ -662,4 +663,20 @@ func TestSet_EqualSlice(t *testing.T) {
 		b := []int{1, 2, 2, 3, 3, 4, 5}
 		must.False(t, a.EqualSlice(b))
 	})
+}
+
+func TestSetSerialization(t *testing.T) {
+	set := NewHashSet[*company, string](10)
+	set.InsertSlice([]*company{c1, c2, c3})
+	bs, err := json.Marshal(set)
+	must.NoError(t, err)
+	must.StrContains(t, string(bs), `"street":1`)
+	must.StrContains(t, string(bs), `"street":2`)
+	must.StrContains(t, string(bs), `"street":3`)
+
+	dstSet := NewHashSet[*company, string](10)
+	err = json.Unmarshal(bs, dstSet)
+	must.NoError(t, err)
+	must.MapEqual(t, dstSet.items, set.items)
+
 }
