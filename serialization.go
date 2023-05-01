cd18377753a089fb89756a8d8aa9cc1a0e2fa473
zonewave <yibo.zhang@centurygame.com>
2023-05-01 22:23:29 +0800

    feat(set): init a common set interface

--- serialization.go
+++ serialization.go
@@ -5,19 +5,13 @@ package set
 
 import "encoding/json"
 
-// serializable is an interface that allows a set to be serialized
-type serializable[T any] interface {
-	Slice() []T
-	InsertSlice([]T) bool
-}
-
 // marshalJSON will serialize a Serializable[T] into a json byte array
-func marshalJSON[T any](s serializable[T]) ([]byte, error) {
+func marshalJSON[T any](s CommonSet[T]) ([]byte, error) {
 	return json.Marshal(s.Slice())
 }
 
 // unmarshalJSON will deserialize a json byte array into a Serializable[T]
-func unmarshalJSON[T any](s serializable[T], data []byte) error {
+func unmarshalJSON[T any](s CommonSet[T], data []byte) error {
 	slice := make([]T, 0)
 	err := json.Unmarshal(data, &slice)
 	if err != nil {
