24d6066c8ae6f8de071e994680f7de29ea74c0c5
zonewave <yibo.zhang@centurygame.com>
2023-05-04 01:39:18 +0800

    lint: 1. sort imports 2.rename commonSet to Common 3.add doc for common 4.rename: Transform to  TransformUnion

--- common.go
+++ common.go
@@ -1,25 +1,31 @@
 package set
 
-// CommonSet is the interface that all sets implement
-type CommonSet[T any] interface {
+// Common is the interface that all sets implement
+type Common[T any] interface {
+	// Slice returns a slice of all elements in the set
 	Slice() []T
+	// Insert inserts an element into the set
+	// if the element already exists, it will return false
 	Insert(T) bool
+	// InsertSlice inserts all elements from the slice into the set
 	InsertSlice([]T) bool
+	// Size returns the number of elements in the set
 	Size() int
-	// ForEach  will call the callback function for each element in the set
-	// If the callback returns false, the iteration will stop
+	// ForEach  will call the callback function for each element in the set.
+	// If the callback returns false, the iteration will stop.
+	// Note: iteration order depends on the underlying implementation;
 	ForEach(call func(T) bool)
 }
 
 // InsertSliceFunc inserts all elements from the slice into the set
-func InsertSliceFunc[T, E any](set CommonSet[T], items []E, f func(element E) T) {
+func InsertSliceFunc[T, E any](s Common[T], items []E, f func(element E) T) {
 	for _, item := range items {
-		set.Insert(f(item))
+		s.Insert(f(item))
 	}
 }
 
-// Transform transforms the set a into another set b
-func Transform[T, E any](a CommonSet[T], b CommonSet[E], transform func(T) E) {
+// TransformUnion transforms the set A into another set B
+func TransformUnion[T, E any](a Common[T], b Common[E], transform func(T) E) {
 	a.ForEach(func(item T) bool {
 		_ = b.Insert(transform(item))
 		return true
@@ -27,7 +33,7 @@ func Transform[T, E any](a CommonSet[T], b CommonSet[E], transform func(T) E) {
 }
 
 // TransformSlice transforms the set into a slice
-func TransformSlice[T, E any](s CommonSet[T], transform func(T) E) []E {
+func TransformSlice[T, E any](s Common[T], transform func(T) E) []E {
 	slice := make([]E, 0, s.Size())
 	s.ForEach(func(item T) bool {
 		slice = append(slice, transform(item))
--- serialization.go
+++ serialization.go
@@ -6,12 +6,12 @@ package set
 import "encoding/json"
 
 // marshalJSON will serialize a Serializable[T] into a json byte array
-func marshalJSON[T any](s CommonSet[T]) ([]byte, error) {
+func marshalJSON[T any](s Common[T]) ([]byte, error) {
 	return json.Marshal(s.Slice())
 }
 
 // unmarshalJSON will deserialize a json byte array into a Serializable[T]
-func unmarshalJSON[T any](s CommonSet[T], data []byte) error {
+func unmarshalJSON[T any](s Common[T], data []byte) error {
 	slice := make([]T, 0)
 	err := json.Unmarshal(data, &slice)
 	if err != nil {
