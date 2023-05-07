71a442137493c47641c661a6c234a5bdb7c423ed
Seth Hoenig <shoenig@duck.com>
2023-05-07 02:33:53 +0000

    treeset: avoid using goroutine / channel / context for iteration
    This PR swaps implementations of TreeSet.iterate to not require a background
    goroutine, channel, and context. Instead we use an explicit stack and a
    generator function to "pull" values one at time. Unlike with the goroutine,
    the stack object is garbage collected automatically if we do not traverse
    the entire tree.
    
    Inspiriation is from https://github.com/golang/go/discussions/56413

--- go.mod
+++ go.mod
@@ -2,9 +2,6 @@ module github.com/hashicorp/go-set
 
 go 1.18
 
-require (
-	github.com/shoenig/test v0.6.4
-	go.uber.org/goleak v1.2.1
-)
+require github.com/shoenig/test v0.6.4
 
 require github.com/google/go-cmp v0.5.9 // indirect
---
+++ stack.go
@@ -0,0 +1,33 @@
+package set
+
+type stack[T any] struct {
+	top *object[T]
+}
+
+type object[T any] struct {
+	item T
+	next *object[T]
+}
+
+func makeStack[T any]() *stack[T] {
+	return new(stack[T])
+}
+
+func (s *stack[T]) push(item T) {
+	obj := &object[T]{
+		item: item,
+		next: s.top,
+	}
+	s.top = obj
+}
+
+func (s *stack[T]) pop() T {
+	obj := s.top
+	s.top = obj.next
+	obj.next = nil
+	return obj.item
+}
+
+func (s *stack[T]) empty() bool {
+	return s.top == nil
+}
